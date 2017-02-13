2a75482d26f22a5794927f907e206765e413c6c6
Armour <497052684@qq.com>
2017-02-13 01:49:57 -0800

    updated Header.tsx

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -9,12 +9,16 @@ class Header extends React.Component<IHeaderProps, IHeaderState> {
   public render() {
     return (
       <nav>
-        <div className="nav-wrapper">
-          <span className="brand-logo"><NavLink to="/" onlyActiveOnIndex={true}>Logo</NavLink></span>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li key="page1"><NavLink to="/page1">Page 1</NavLink></li>
-            <li key="page2"><NavLink to="/page2">Page 2</NavLink></li>
-          </ul>
+        <div className="row">
+          <div className="col s12">
+            <div className="nav-wrapper">
+              <span className="brand-logo"><NavLink to="/" onlyActiveOnIndex={true}>Logo</NavLink></span>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li key="page1"><NavLink to="/page1">Page 1</NavLink></li>
+                <li key="page2"><NavLink to="/page2">Page 2</NavLink></li>
+              </ul>
+            </div>
+          </div>
         </div>
       </nav>
     );
