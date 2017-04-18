2fb93b00221accdd5e3cba82229bbc451fd4b88c
Armour <497052684@qq.com>
2017-04-18 01:54:35 -0700

    fixed 'React is not defined' problem

--- frontend/src/containers/DevTools.tsx
+++ frontend/src/containers/DevTools.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createDevTools } from 'redux-devtools';
 import DockMonitor from 'redux-devtools-dock-monitor';
 import LogMonitor from 'redux-devtools-log-monitor';
