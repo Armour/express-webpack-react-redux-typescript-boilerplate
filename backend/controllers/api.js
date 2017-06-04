7e54d5b98a4ea91593cd606b5e7d3f5370da23a2
Armour <497052684@qq.com>
2017-06-04 15:07:12 -0700

    use pg-promise instead of pg

--- backend/controllers/api.js
+++ backend/controllers/api.js
@@ -1,30 +1,18 @@
-import pg from 'pg';
+import { db } from '../lib/db';
 
-import config from '../config.json';
-
-const pgConnect = `postgres://${config.pgsql_username}:${config.pgsql_password}@${config.pgsql_hostname}/${config.pgsql_database}`;
-
-export const apiRequest = (req, res, requestType) => {
-  pg.connect(pgConnect, (err, client, done) => {
-    if (err) {
-      console.error('PostgreSQL ERROR : %s', err.message);
-      if (client) done(client);
-    } else {
-      client.query('SELECT * FROM app_name_modelname', (err2, result) => {
-        if (err) {
-          console.error('PostgreSQL ERROR : %s', err2.message);
-          if (client) done(client);
-        } else {
-          done();
-          const data = result.rowCount ? result.rows[0].field_name : 'data not found, check your database';
-          res.json({
-            data,
-            requestType,
-          });
-        }
-      });
-    }
-  });
+export const apiRequest = async (req, res, requestType) => {
+  try {
+    const data = await db.one('SELECT * FROM app_name_modelname', []);
+    res.json({
+      data: data.rows[0].field_name,
+      requestType,
+    });
+  } catch (e) {
+    res.json({
+      data: 'data not found, check your database',
+      requestType,
+    });
+  }
 };
 
 export const apiGet = (req, res) => apiRequest(req, res, 'GET');
---
+++ backend/lib/db.js
@@ -0,0 +1,15 @@
+import pgp from 'pg-promise';
+
+import config from '../config.json';
+
+// Create a config to configure both pooling behavior and client options
+// note: all config is optional and the environment variables will be read if the config is not present
+const dbConfig = {
+  host: config.pgsql_hostname, // Server hosting the postgres database
+  database: config.pgsql_database, // env var: PGDATABASE
+  user: config.pgsql_username, // env var: PGUSER
+  password: config.pgsql_password, // env var: PGPASSWORD
+};
+
+// Initializes a connection pool
+export const db = pgp()(dbConfig);
