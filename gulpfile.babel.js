f1d7b6ab068098516a93195e84ece22bab349569
Chong Guo <armourg@sfu.ca>
2017-10-19 17:41:03 -0700

    format gulpfile, remove duplicated code

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,19 +1,18 @@
 import fs from 'fs';
-import gulp from 'gulp';
 import del from 'del';
 import runSequence from 'run-sequence';
 import childProcess from 'child_process';
+import gulp from 'gulp';
 import eslint from 'gulp-eslint';
 import tslint from 'gulp-tslint';
 import stylelint from 'gulp-stylelint';
-import unzip from 'gulp-unzip';
-import zip from 'gulp-zip';
 
 const { spawn } = childProcess;
 const isProduction = process.env.NODE_ENV === 'production';
 const yarn = process.platform === 'win32' ? 'yarn.cmd' : 'yarn';
 
 const path = {
+  dist: 'frontend/dist',
   src: {
     js: '**/*.js',
     jsx: '**/*.jsx',
@@ -27,24 +26,38 @@ const path = {
   },
 };
 
+const tasks = {
+  eslint: 'eslint',
+  tslint: 'tslint',
+  stylelint: 'stylelint',
+  clean: 'clean',
+  buildDll: 'yarn:run-build-dll',
+  buildProd: 'yarn:run-build-prod',
+  profile: 'yarn:run-profile',
+  server: 'yarn:run-server',
+  test: 'yarn:run-test',
+  coverage: 'yarn:run-coverage',
+  deploy: 'yarn:run-deploy',
+  build: 'build',
+  start: 'build & run server',
+};
+
 // Run eslint
-gulp.task('eslint', () =>
+gulp.task(tasks.eslint, () =>
   gulp.src([path.src.js, path.src.jsx]) // respect .eslintignore
     .pipe(eslint())
     .pipe(eslint.format('codeframe'))
     .pipe(eslint.failAfterError()));
 
 // Run tslint
-gulp.task('tslint', () =>
+gulp.task(tasks.tslint, () =>
   gulp.src([path.src.ts, path.src.tsx, path.exclude.node_modules])
-    .pipe(tslint({
-      formatter: 'codeFrame',
-    }))
+    .pipe(tslint({ formatter: 'codeFrame' }))
     .pipe(tslint.report()));
 
 // Run stylelint
-gulp.task('stylelint', () =>
-  gulp.src([path.src.css, path.src.scss]) // respect .stylelintignore
+gulp.task(tasks.stylelint, () =>
+  gulp.src([path.src.css, path.src.scss]) // respect ignoreFiles in .stylelintrc
     .pipe(stylelint({
       reporters: [
         { formatter: 'string', console: true },
@@ -52,78 +65,52 @@ gulp.task('stylelint', () =>
     })));
 
 // Clean webpack generated files
-gulp.task('clean', () => del(['frontend/dist', 'frontend/dist.zip', '.awcache']));
+gulp.task(tasks.clean, () => del([path.dist, '.awcache', 'coverage']));
 
-// Pack generated dist file for depoyment
-gulp.task('pack', () =>
-  gulp.src('frontend/dist/**')
-    .pipe(zip('dist.zip'))
-    .pipe(gulp.dest('frontend')));
-
-// Unpack generated dist file for depoyment
-gulp.task('unpack', () =>
-  gulp.src('frontend/dist.zip')
-    .pipe(unzip())
-    .pipe(gulp.dest('frontend/dist')));
+// Yarn task generator
+const generateTask = (taskName, args) => {
+  gulp.task(taskName, (callback) => {
+    const proc = spawn(yarn, args, { stdio: 'inherit' });
+    proc.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+      callback();
+    });
+    proc.on('error', (err) => {
+      callback(err);
+    });
+  });
+};
 
 // Build dll reference files
-gulp.task('webpack:build-dll', (callback) => {
-  const buildDll = spawn(yarn, ['run', 'build-dll'], { stdio: 'inherit' });
-  buildDll.on('close', (code) => {
-    console.log(`build-dll child process exited with code ${code}`);
-    callback();
-  });
-  buildDll.on('error', (err) => {
-    callback(err);
-  });
-});
+generateTask(tasks.buildDll, ['run', 'build-dll']);
 
 // Generate webpack asset bundles for production
-gulp.task('webpack:build-prod', (callback) => {
-  const buildProd = spawn(yarn, ['run', 'build-prod'], { stdio: 'inherit' });
-  buildProd.on('close', (code) => {
-    console.log(`build-prod child process exited with code ${code}`);
-    callback();
-  });
-  buildProd.on('error', (err) => {
-    callback(err);
-  });
-});
+generateTask(tasks.buildProd, ['run', 'build-prod']);
 
 // Profile webpack asset bundle
-gulp.task('webpack:profile', (callback) => {
-  const profile = spawn(yarn, ['run', 'profile'], { stdio: 'inherit' });
-  profile.on('close', (code) => {
-    console.log(`profile child process exited with code ${code}`);
-    callback();
-  });
-  profile.on('error', (err) => {
-    callback(err);
-  });
-});
+generateTask(tasks.profile, ['run', 'profile']);
 
 // Run server
-gulp.task('express:run-server', (callback) => {
-  const runServer = spawn(yarn, ['run', 'server'], { stdio: 'inherit' });
-  runServer.on('close', (code) => {
-    console.log(`run-server child process exited with code ${code}`);
-    callback();
-  });
-  runServer.on('error', (err) => {
-    callback(err);
-  });
-});
+generateTask(tasks.server, ['run', 'server']);
+
+// Test
+generateTask(tasks.test, ['run', 'test']);
+
+// Coverage
+generateTask(tasks.coverage, ['run', 'coverage']);
+
+// Deployment
+generateTask(tasks.deploy, ['run', 'deploy']);
 
 // Generate asset bundles
-gulp.task('build', (callback) => {
-  fs.exists('frontend/dist', (exists) => {
-    const taskList = [
-    ];
+gulp.task(tasks.build, (callback) => {
+  fs.exists(path.dist, (exists) => {
+    const taskList = [];
     if (isProduction) {
-      taskList.unshift('webpack:build-prod');
+      taskList.unshift(tasks.buildProd);
     }
     if (!exists) {
-      taskList.unshift('webpack:build-dll');
+      taskList.unshift(tasks.buildDll);
     }
     if (taskList.length > 0) {
       runSequence(...taskList, callback);
@@ -134,57 +121,8 @@ gulp.task('build', (callback) => {
 });
 
 // Generate asset bundles and run server
-gulp.task('build & run', (callback) => {
-  runSequence('build', 'express:run-server', callback);
-});
-
-// Profiling
-gulp.task('profile', (callback) => {
-  fs.exists('frontend/dist', (exists) => {
-    const taskList = [
-      'webpack:profile',
-    ];
-    if (!exists) {
-      taskList.unshift('webpack:build-dll');
-    }
-    runSequence(...taskList, callback);
-  });
-});
-
-// Test
-gulp.task('test', (callback) => {
-  const buildProd = spawn(yarn, ['run', 'test'], { stdio: 'inherit' });
-  buildProd.on('close', (code) => {
-    console.log(`test child process exited with code ${code}`);
-    callback();
-  });
-  buildProd.on('error', (err) => {
-    callback(err);
-  });
-});
-
-// Coveralls
-gulp.task('coveralls', (callback) => {
-  const buildProd = spawn(yarn, ['run', 'coveralls'], { stdio: 'inherit' });
-  buildProd.on('close', (code) => {
-    console.log(`coveralls child process exited with code ${code}`);
-    callback();
-  });
-  buildProd.on('error', (err) => {
-    callback(err);
-  });
-});
-
-// Deployment
-gulp.task('deploy', (callback) => {
-  const buildProd = spawn(yarn, ['run', 'deploy'], { stdio: 'inherit' });
-  buildProd.on('close', (code) => {
-    console.log(`deploy child process exited with code ${code}`);
-    callback();
-  });
-  buildProd.on('error', (err) => {
-    callback(err);
-  });
+gulp.task(tasks.start, (callback) => {
+  runSequence(tasks.build, tasks.server, callback);
 });
 
 // Default task
@@ -193,5 +131,5 @@ gulp.task('deploy', (callback) => {
 // 3. stylelint
 // 4. generate asset bundles and run server
 gulp.task('default', (callback) => {
-  runSequence('eslint', 'tslint', 'stylelint', 'build & run', callback);
+  runSequence(tasks.eslint, tasks.tslint, tasks.stylelint, tasks.start, callback);
 });
